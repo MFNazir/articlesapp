c381a411ab822b5da030dabba5fc9cff8006cdef
Muhammed Nazir <mnazir9876@gmail.com>
2023-08-11 11:20:07 -0500

    adding more mfa restrictions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action -> { rodauth.require_authentication }
+  before_action -> { rodauth.require_two_factor_setup }
   before_action :set_article, only: %i[ show edit update destroy ]
 
   # GET /articles
